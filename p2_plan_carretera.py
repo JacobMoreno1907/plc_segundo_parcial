09fdc280bebb6d73f40e7640aca1e731fbaf9d40
JacobMoreno1907 <145026001+JacobMoreno1907@users.noreply.github.com>
2023-10-15 13:02:46 -0600

    p2_plan_carretera.py

--- p2_plan_carretera.py
+++ p2_plan_carretera.py
@@ -1,1 +1,11 @@
-.
+#https://replit.com/@Jacob-OlafOlaf/Planificacion-de-Viaje-por-Carretera?v=1
+millas = float(input("Ingrese la distancia en millas: "))
+rendimiento = float(input("¿Cuál es el rendimiento de millas por galón de gasolina? "))
+precio = float(input("¿Cuál es el precio actual del galón de gasolina? "))
+dias = float(input("¿Cuántos días planeas de viaje? "))
+if dias >= 3:
+    precio *= 1.5
+else:
+    precio *= 1
+costo_total = (millas / rendimiento) * precio * dias
+print("El costo de todo el viaje es de:", costo_total)
